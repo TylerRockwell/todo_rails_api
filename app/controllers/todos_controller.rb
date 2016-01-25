a5edc0a3569705fd0a9389a87a9c9db9a76edfe3
Tyler Rockwell <tyler.rockwell87@gmail.com>
2016-01-25 14:55:41 -0500

    Implement TodoController update and destroy actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:update, :destroy]
   def index
     @todos = Todo.all
 
@@ -16,12 +17,26 @@ class TodosController < ApplicationController
   end
 
   def update
+    @todo.update(todo_params)
+    if @todo.save
+      render json: { todo: @todo.as_json }
+    else
+      render json: { error: "Could not save todo item" }
+    end
   end
 
   def destroy
+    @todo.destroy
+    render json: {}
   end
+
+  private
 
   def todo_params
     params.require(:todo).permit(:title, :is_completed)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
   end
 end
