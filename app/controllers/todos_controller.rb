903b3b2d92f35240e049ad156db50a2fa086548f
Tyler Rockwell <tyler.rockwell87@gmail.com>
2016-01-25 14:26:59 -0500

    Implement TodoController index action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,12 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
+
+    render json: { todos: @todos.as_json }
   end
 
   def create
+    @todo = Todo.new()
   end
 
   def update
